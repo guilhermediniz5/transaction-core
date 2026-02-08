b614c339efd8f83cfae9a07f91c9e5d9bb88d8a4
João Guilherme <jaumzin2020@gmail.com>
2026-02-08 01:30:12 -0300

    Fix => SQL de acordo com o Postgres Fix => Leitura da env na conexão com docker

--- db/init.sql
+++ db/init.sql
@@ -1,20 +1,20 @@
 CREATE TABLE users 
 ( 
- id PRIMARY KEY SERIAL,  
+ id SERIAL PRIMARY KEY,  
  nome VARCHAR(100) NOT NULL,  
  email VARCHAR(100) NOT NULL,  
- saldo INT NOT NULL,  
+ saldo INT NOT NULL
 ); 
 
 CREATE TABLE transactions 
 ( 
- id PRIMARY KEY SERIAL,  
+ id SERIAL PRIMARY KEY,  
  from_user INT NOT NULL,  
  to_user INT NOT NULL,  
  valor INT NOT NULL,  
  status VARCHAR(20) NOT NULL,  
- created_at TIMESTAMP NOT NULL,  
+ created_at TIMESTAMP NOT NULL
 ); 
 
-ALTER TABLE transactions ADD FOREIGN KEY(from_user) REFERENCES users (id)
-ALTER TABLE transactions ADD FOREIGN KEY(to_user) REFERENCES users (id)
+ALTER TABLE transactions ADD FOREIGN KEY(from_user) REFERENCES users (id);
+ALTER TABLE transactions ADD FOREIGN KEY(to_user) REFERENCES users (id);
--- src/config/pool.ts
+++ src/config/pool.ts
@@ -1,7 +1,9 @@
 import pkg from "pg";
+import dotenv from "dotenv";
 
 const { Pool } = pkg;
+dotenv.config();
 
-const pool = new Pool({
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
