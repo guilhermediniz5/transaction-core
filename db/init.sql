9605083fc94fc5b2d3dc8995aafc190fc5e2ffa6
João Guilherme <jaumzin2020@gmail.com>
2026-02-07 23:39:40 -0300

    Add => Base do projeto

---
+++ db/init.sql
@@ -0,0 +1,20 @@
+CREATE TABLE users 
+( 
+ id PRIMARY KEY SERIAL,  
+ nome VARCHAR(100) NOT NULL,  
+ email VARCHAR(100) NOT NULL,  
+ saldo INT NOT NULL,  
+); 
+
+CREATE TABLE transactions 
+( 
+ id PRIMARY KEY SERIAL,  
+ from_user INT NOT NULL,  
+ to_user INT NOT NULL,  
+ valor INT NOT NULL,  
+ status VARCHAR(20) NOT NULL,  
+ created_at TIMESTAMP NOT NULL,  
+); 
+
+ALTER TABLE transactions ADD FOREIGN KEY(from_user) REFERENCES users (id)
+ALTER TABLE transactions ADD FOREIGN KEY(to_user) REFERENCES users (id)
